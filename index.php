a5109faa256ae54f5a7d4578f0160b12b196c452
lttpiii <150350390+lttpiii@users.noreply.github.com>
2024-10-07 14:28:46 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMK 5 KOTA BEKASI</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-white w-full h-full">
+    <div class="fixed top-0 w-full bg-black flex justify-center items-center space-x-8 h-12">
+        <div class="">
+            <a href="#">
+                <p class="text-white hover:text-gray-400 hover:translate-y-1 hover:delay-100">Home</p>
+            </a>
+        </div>
+        <div class="">
+            <a href="about.html">
+                <p class="text-white hover:text-gray-400 hover:translate-y-1 hover:delay-100">About</p>
+            </a>
+        </div>
+        <div class="">
+            <a href="contact.html">
+                <p class="text-white hover:text-gray-400 hover:translate-y-1 hover:delay-100">Contact</p>
+            </a>
+        </div>
+        <div class="">
+            <a href="formDaftar.php">
+                <p class="text-white hover:text-gray-400 hover:translate-y-1 hover:delay-100">Pendaftaran</p>
+            </a>
+        </div>
+        
+    </div>
+    <div class="flex justify-center items-center h-screen">
+        <div class="flex justify-center ">
+            <div class="bg-white border-2 mt-5 w-96 p-2 rounded-xl shadow-2xl">
+                <div class=" text-center mt-8 font-bold ">
+                    <h1 class="text-2xl">SMKN 5 KOTA BEKASI</h1>
+                </div>
+                <div class="mt-12 w-full flex justify-center">
+                    <img class="size-64" src="download.jpeg" alt="">
+                </div>
+                <div class="my-2">
+                    <p class="font-semibold font-serif text-wrap text-base text-center mt-8">Sekolah coding terbaik di dunia, dengan 100% lulusan menjadi programmer ahli.</p>
+                </div>
+            </div> 
+        </div>
+    </div>
+
+    <div class="bg-black flex justify-center items-center fixed w-full bottom-0 h-12 ">
+        <p class="font-light text-white">@COPYRIGHT BY EL-BANTAI</p>
+    </div>
+</body>
+</html>
